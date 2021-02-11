892080a32c41b063b7c69898ecc806e581187809
2cloud <61458657+2cloudfze@users.noreply.github.com>
2021-02-11 14:25:52 +0530

    Delete metadata.sh

--- metadata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-        --profile s3-sync-action \
-        --no-progress \
-	      --content-type 'text/html' \
-	      --exclude '*.*' \
-        ${ENDPOINT_APPEND} $*"
