7220aad6b4e57ae8fe8f41e518b9765613a1f1ea
2cloud <61458657+2cloudfze@users.noreply.github.com>
2021-02-13 23:47:24 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,7 +43,6 @@ sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-sync-action \
               --no-progress \
               ${ENDPOINT_APPEND} $*" && echo "first sync done "
-sleep 1
 sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
         --profile s3-sync-action \
         --no-progress \
