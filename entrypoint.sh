a394ec0d4fdb6699ca243eb60cab76770ac25bf9
2cloud <61458657+2cloudfze@users.noreply.github.com>
2021-02-14 17:08:21 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,12 +30,12 @@ fi
 # Create a dedicated profile for this action to avoid conflicts
 # with past/future actions.
 # https://github.com/jakejarvis/s3-sync-action/issues/1
-aws configure --profile s3-sync-metadata <<-EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_REGION}
-text
-EOF
+#aws configure --profile s3-sync-metadata <<-EOF > /dev/null 2>&1
+#${AWS_ACCESS_KEY_ID}
+#${AWS_SECRET_ACCESS_KEY}
+#${AWS_REGION}
+#text
+#EOF
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
@@ -51,9 +51,9 @@ echo "test"
 # We need to re-run `aws configure` with bogus input instead of
 # deleting ~/.aws in case there are other credentials living there.
 # https://forums.aws.amazon.com/thread.jspa?threadID=148833
-aws configure --profile s3-sync-metadata <<-EOF > /dev/null 2>&1
-null
-null
-null
-text
-EOF
+#aws configure --profile s3-sync-metadata <<-EOF > /dev/null 2>&1
+#null
+#null
+#null
+#text
+#EOF
