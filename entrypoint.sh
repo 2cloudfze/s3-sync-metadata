f97b86e23eec56de167d6b2ec75aa01de2a749ed
2cloud <61458657+2cloudfze@users.noreply.github.com>
2021-02-14 18:55:46 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,10 @@ sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-sync-action \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
-
+sh -c "aws s3 cp ${SOURCE_DIR:-.}/about s3://${AWS_S3_BUCKET}/${DEST_DIR}/about \
+              --profile s3-sync-action \
+              --no-progress \
+              ${ENDPOINT_APPEND} $*"
 	
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
